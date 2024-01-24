651c6dfb352e91c188753986094342b7bbbfb4db
DomingosSAmpah <alfredosamp99@gmail.com>
2024-01-24 07:06:48 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+// Script header
+
+$(document).ready(function(){
+    $('.fa-bars').click(function(params) {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+    $(window).on('scroll load', function (params) {
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if ($(window).scrollTop() > 30) {
+            $('header').addClass('header-active');
+        }else{
+            $('header').removeClass('header-active');
+        }
+        // Menu top-fixo
+        $('section').each(function (params) {
+            var top = $(window).scrollTop();
+            var id = $(this).attr('id');
+            var height = $(this).height();
+            var top = $(this).offset().top - 200;
+            if (top >= offset && top < height + offset) {
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find('[href="#' + id + '"]').addClass('active');
+            }
+        });
+    });
+
+
+});
